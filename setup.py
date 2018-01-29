1612de94f6f07d221c90f797260f0fb99886ac77
AndrewHerzing <aaherzing@gmail.com>
2018-01-29 16:20:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version='0.1.0',
     author='Andrew A. Herzing',
     description='Suite of data processing algorithems for processing electron tomography data',
-    long_description=open('README.txt').read(),
     install_requires=[
         "Hyperspy >= 1.0",
         "numpy",
