459a356b19b7391abbd6edb8a62be6b06f9b6631
AndrewHerzing <aaherzing@gmail.com>
2018-01-31 13:26:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
     name='tomotools',
     version='0.1.0',
     author='Andrew A. Herzing',
     description='Suite of data processing algorithems for processing electron tomography data',
-    install_requires=[
-        "hyperspy >= 1.0",
-        "numpy",
-        "scipy",
-        "matplotlib",
-        "astra-toolbox",
-        "tqdm"
+    packages = find_packages()
     ],
 )
