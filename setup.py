a387682ce404092f91b26ab2762f0e3d01620fe1
AndrewHerzing <aaherzing@gmail.com>
2018-01-31 13:27:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(
     version='0.1.0',
     author='Andrew A. Herzing',
     description='Suite of data processing algorithems for processing electron tomography data',
-    packages = find_packages()
-    ],
+    packages = find_packages(),
 )
