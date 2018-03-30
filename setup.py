010616fb21963790e267c8d2764c8ab7918dec26
AndrewHerzing <aaherzing@gmail.com>
2018-03-30 17:06:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='tomotools',
-    version='0.1.0',
+    version='0.2.0',
     author='Andrew A. Herzing',
     description='Suite of data processing algorithems for processing electron tomography data',
     packages = find_packages(),
