932b41cc6eee3d4ab4f5dcf189a26f9c19b6623c
AndrewHerzing <aaherzing@gmail.com>
2019-03-01 14:25:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(
     version='0.2.0',
     author='Andrew A. Herzing',
     description='Suite of data processing algorithms for processing electron tomography data',
-    packages=find_packages(), install_requires=['numpy', 'qt', 'hyperspy', 'opencv', 'tqdm', 'matplotlib', 'tomopy',
-                                                'astra-toolbox', 'scipy']
+    packages=find_packages()
 )
