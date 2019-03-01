6c5ca6018a546080278f3d83aba8324de387f9ff
aaherzing <aaherzing@gmail.com>
2019-03-01 15:22:56 -0500

    Repaired error in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tomotools',
-    version='0.2.0',
-    author='Andrew A. Herzing',
-    description='Suite of data processing algorithms for processing electron tomography data',
-    packages=find_packages()
+    name = 'tomotools',
+    version = '0.2.0',
+    author = 'Andrew A. Herzing',
+    description = 'Suite of data processing algorithms for processing electron tomography data',
+    packages = [
+        'tomotools',
+        ],
+    install_requires = [
+        'hyperspy',
+        'numpy',
+        'astra-toolbox',
+        'scipy',
+        'tqdm',
+        'matplotlib',
+        'tomopy',
+    ],
 )
