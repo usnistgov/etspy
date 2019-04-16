9015273ec569c66e4cb066429a426bde15733379
Andrew Herzing <aaherzing@gmail.com>
2019-04-16 18:35:22 +0000

    Delete hyperspy_log.py

--- hyperspy_log.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python 
-# ============================
-# 2019-04-03 
-# 13:26 
-# ============================
-quit()
-s = tomotools.load('tomotools/tests/test_data/HAADF.mrc')
-ali = s.stack_register('ECC')
-ali.show()
