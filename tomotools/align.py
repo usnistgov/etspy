bc9fd09f882fe2fcee2aca3467e0473e08958f9e
Herzing <aherzing@NIST.GOV>
2019-04-17 16:25:15 -0400

    Fixed_error_in_findTransformECC_missing_inputMask_caused_PyTest_Error

--- tomotools/align.py
+++ tomotools/align.py
@@ -95,7 +95,8 @@ def align_stack(stack, method, start, show_progressbar):
                     np.float32(stack.data[i+1, :, :]),
                     warp_matrix,
                     cv2.MOTION_TRANSLATION,
-                    criteria)
+                    criteria,
+                    None)
                 shifts[i, :] = trans[:, 2]
 
         if start != 0:
