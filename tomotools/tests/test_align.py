26be5470f402eb0247f4a7a4540814aa9d5281dc
AndrewHerzing <aaherzing@gmail.com>
2020-04-07 12:07:55 -0400

    Fixed path issues in tests

--- tomotools/tests/test_align.py
+++ tomotools/tests/test_align.py
@@ -2,13 +2,13 @@ import tomotools.api as tomotools
 import os
 import pytest
 
-my_path = os.path.dirname(__file__)
+tomotools_path = os.path.dirname(tomotools.__file__)
 
 
 class TestAlignStackRegister:
 
     def test_register_ecc(self):
-        filename = os.path.join(my_path, "test_data", "HAADF.mrc")
+        filename = os.path.join(tomotools_path, "test_data", "HAADF.mrc")
         stack = tomotools.load(filename)
         reg = stack.inav[0:20].stack_register('ECC')
         assert type(reg) is tomotools.TomoStack
@@ -18,7 +18,7 @@ class TestAlignStackRegister:
             stack.inav[0:20].axes_manager.navigation_shape
 
     def test_register_pc(self):
-        filename = os.path.join(my_path, "test_data", "HAADF.mrc")
+        filename = os.path.join(tomotools_path, "test_data", "HAADF.mrc")
         stack = tomotools.load(filename)
         reg = stack.inav[0:20].stack_register('PC')
         assert type(reg) is tomotools.TomoStack
@@ -28,7 +28,7 @@ class TestAlignStackRegister:
             stack.inav[0:20].axes_manager.navigation_shape
 
     def test_register_com(self):
-        filename = os.path.join(my_path, "test_data", "HAADF.mrc")
+        filename = os.path.join(tomotools_path, "test_data", "HAADF.mrc")
         stack = tomotools.load(filename)
         reg = stack.inav[0:20].stack_register('COM')
         assert type(reg) is tomotools.TomoStack
@@ -38,7 +38,7 @@ class TestAlignStackRegister:
             stack.inav[0:20].axes_manager.navigation_shape
 
     def test_register_stackreg(self):
-        filename = os.path.join(my_path, "test_data", "HAADF.mrc")
+        filename = os.path.join(tomotools_path, "test_data", "HAADF.mrc")
         stack = tomotools.load(filename)
         reg = stack.inav[0:20].stack_register('StackReg')
         assert type(reg) is tomotools.TomoStack
@@ -48,13 +48,13 @@ class TestAlignStackRegister:
             stack.inav[0:20].axes_manager.navigation_shape
 
     def test_register_unknown_method(self):
-        filename = os.path.join(my_path, "test_data", "HAADF.mrc")
+        filename = os.path.join(tomotools_path, "test_data", "HAADF.mrc")
         stack = tomotools.load(filename)
         with pytest.raises(ValueError):
             stack.inav[0:20].stack_register('WRONG')
 
     def test_tilt_align_com(self):
-        filename = os.path.join(my_path, "test_data", "HAADF.mrc")
+        filename = os.path.join(tomotools_path, "test_data", "HAADF.mrc")
         stack = tomotools.load(filename)
         stack.axes_manager[0].offset = -76
         stack.axes_manager[0].scale = 2
@@ -64,7 +64,7 @@ class TestAlignStackRegister:
         assert 100 * abs((-2.3 - tilt_axis) / -2.3) < 1.0
 
     def test_tilt_align_maximage(self):
-        filename = os.path.join(my_path, "test_data", "HAADF.mrc")
+        filename = os.path.join(tomotools_path, "test_data", "HAADF.mrc")
         stack = tomotools.load(filename)
         stack.axes_manager[0].offset = -76
         stack.axes_manager[0].scale = 2
@@ -74,13 +74,13 @@ class TestAlignStackRegister:
         assert abs(-2.3 - tilt_axis) < 1.0
 
     def test_tilt_align_unknown_method(self):
-        filename = os.path.join(my_path, "test_data", "HAADF.mrc")
+        filename = os.path.join(tomotools_path, "test_data", "HAADF.mrc")
         stack = tomotools.load(filename)
         with pytest.raises(ValueError):
             stack.tilt_align(method='WRONG')
 
     def test_align_to_other(self):
-        filename = os.path.join(my_path, "test_data", "HAADF.mrc")
+        filename = os.path.join(tomotools_path, "test_data", "HAADF.mrc")
         stack = tomotools.load(filename)
         stack2 = stack.deepcopy()
         reg = stack.stack_register('PC')
@@ -89,7 +89,7 @@ class TestAlignStackRegister:
         assert diff.sum() == 0.0
 
     def test_align_to_other_no_alignment(self):
-        filename = os.path.join(my_path, "test_data", "HAADF.mrc")
+        filename = os.path.join(tomotools_path, "test_data", "HAADF.mrc")
         stack = tomotools.load(filename)
         stack2 = stack.deepcopy()
         reg = stack.deepcopy()
@@ -97,7 +97,7 @@ class TestAlignStackRegister:
             reg.align_other(stack2)
 
     def test_align_to_other_with_crop(self):
-        filename = os.path.join(my_path, "test_data", "HAADF.mrc")
+        filename = os.path.join(tomotools_path, "test_data", "HAADF.mrc")
         stack = tomotools.load(filename)
         reg = stack.stack_register('PC', crop=True)
         reg2 = reg.align_other(stack)
