a4f92bcad43eb37ca6d0d7cbed535418b6c2dc4e
AndrewHerzing <aaherzing@gmail.com>
2020-04-07 11:41:20 -0400

    Removed imp due to deprecation

--- tomotools/datasets.py
+++ tomotools/datasets.py
@@ -1,9 +1,8 @@
-import imp
 import tomotools.api as tomotools
 from tomotools.simulation import misalign_stack, add_gaussian_noise
 
 
-def get_needle_data(self, aligned=False):
+def get_needle_data(aligned=False):
     """
     Retrieve experimental tilt series of needle-shaped specimen.
 
@@ -13,7 +12,8 @@ def get_needle_data(self, aligned=False):
         TomoStack containing the simulated catalyst tilt series
 
     """
-    test_data_path = imp.find_module("tomotools")[1] + '\\tests\\test_data\\'
+    test_data_path = os.path.dirname(tomotools.__file__)
+    # test_data_path = imp.find_module("tomotools")[1] + '\\tests\\test_data\\'
     if aligned:
         needle =\
             tomotools.load(test_data_path + 'HAADF_Aligned.hdf5')
@@ -35,7 +35,8 @@ def get_catalyst_tilt_series(misalign=False, minshift=-5, maxshift=5,
         TomoStack containing the simulated catalyst tilt series
 
     """
-    test_data_path = imp.find_module("tomotools")[1] + '\\tests\\test_data\\'
+    test_data_path = os.path.dirname(tomotools.__file__)
+    # test_data_path = imp.find_module("tomotools")[1] + '\\tests\\test_data\\'
     catalyst =\
         tomotools.load(test_data_path + 'Catalyst3DModel_TiltSeries180.hdf5')
     if misalign:
