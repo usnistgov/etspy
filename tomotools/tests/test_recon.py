84369050b01e1e36a12c7e4225b85dd70178d929
AndrewHerzing <aaherzing@gmail.com>
2020-05-12 14:40:34 -0400

    Added tests for astra_sirt and astra_project

--- tomotools/tests/test_recon.py
+++ tomotools/tests/test_recon.py
@@ -1,6 +1,8 @@
 import tomotools
 import os
 from tomotools.io import load
+from tomotools import recon
+import numpy
 
 tomotools_path = os.path.dirname(tomotools.__file__)
 
@@ -31,3 +33,60 @@ class TestReconstruction:
         assert type(rec) is tomotools.base.TomoStack
         assert rec.axes_manager.navigation_shape[0] == \
             slices.axes_manager.signal_shape[1]
+
+
+class TestAstraReconstruction:
+
+    def test_astra_sirt_3d_data(self):
+        filename = os.path.join(tomotools_path, "tests", "test_data",
+                                "HAADF_Aligned.hdf5")
+        stack = load(filename)
+        angles = stack.axes_manager[0].axis
+        slices = stack.isig[:, 120:121].deepcopy().data
+        rec = recon.astra_sirt(slices, angles,
+                               thickness=None, iterations=2,
+                               constrain=True, thresh=0, cuda=False)
+        assert rec.shape == (1, slices.shape[2], slices.shape[2])
+        assert rec.shape[0] == slices.shape[1]
+        assert type(rec) is numpy.ndarray
+
+    def test_astra_sirt_2d_data(self):
+        filename = os.path.join(tomotools_path, "tests", "test_data",
+                                "HAADF_Aligned.hdf5")
+        stack = load(filename)
+        angles = stack.axes_manager[0].axis
+        slices = stack.isig[:, 120].deepcopy().data
+        rec = recon.astra_sirt(slices, angles,
+                               thickness=None, iterations=2,
+                               constrain=True, thresh=0, cuda=False)
+        assert rec.shape == (1, slices.shape[1], slices.shape[1])
+        assert rec.shape[0] == 1
+        assert type(rec) is numpy.ndarray
+
+    def test_astra_project_3d_data(self):
+        filename = os.path.join(tomotools_path, "tests", "test_data",
+                                "HAADF_Aligned.hdf5")
+        stack = load(filename)
+        angles = stack.axes_manager[0].axis
+        slices = stack.isig[:, 120:121].deepcopy().data
+        rec = recon.astra_sirt(slices, angles,
+                               thickness=None, iterations=1,
+                               constrain=True, thresh=0, cuda=False)
+        sino = recon.astra_project(rec, angles)
+        assert sino.shape == (len(angles), rec.shape[0], rec.shape[2])
+        assert sino.shape[1] == slices.shape[1]
+        assert type(sino) is numpy.ndarray
+
+    def test_astra_project_2d_data(self):
+        filename = os.path.join(tomotools_path, "tests", "test_data",
+                                "HAADF_Aligned.hdf5")
+        stack = load(filename)
+        angles = stack.axes_manager[0].axis
+        slices = stack.isig[:, 120].deepcopy().data
+        rec = recon.astra_sirt(slices, angles,
+                               thickness=None, iterations=1,
+                               constrain=True, thresh=0, cuda=False)
+        sino = recon.astra_project(rec, angles)
+        assert sino.shape == (len(angles), rec.shape[0], rec.shape[2])
+        assert sino.shape[1] == 1
+        assert type(sino) is numpy.ndarray
