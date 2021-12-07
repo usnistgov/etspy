151d1da4695b65f10eaf9375f500f7c74bb63047
Andrew Herzing <andrew.herzing@nist.gov>
2021-12-07 14:22:17 -0500

    Fixed assert error in test_sirt_error

--- tomotools/tests/test_base.py
+++ tomotools/tests/test_base.py
@@ -132,5 +132,5 @@ class TestTomoStack:
         assert error.data.shape[0] == rec_stack.data.shape[0]
         assert rec_stack.data.shape[1:] ==\
             (stack.data.shape[2], stack.data.shape[2])
-        assert (1 - (3.8709e12 / error[0])) < 0.001
-        assert (1 - (2.8624e12 / error[1])) < 0.001
+        assert (1 - (3.8709e12 / error.data[0])) < 0.001
+        assert (1 - (2.8624e12 / error.data[1])) < 0.001
