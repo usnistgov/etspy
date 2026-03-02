f837568763f952fbf68e2d822f1bacce7565c0f6
Andrew Herzing <andrew.herzing@nist.gov>
2026-03-02 14:50:48 -0500

    Fixed tilt align tests following align.py refactoring

--- etspy/tests/test_align.py
+++ etspy/tests/test_align.py
@@ -26,89 +26,98 @@ except Exception:
     cupy_in_test_env = False
 
 
+@pytest.fixture(scope="module")
+def short_stack():
+    """Load stack from test data and truncate to 5 images."""
+    return ds.get_needle_data().inav[0:5]
+
+
+@pytest.fixture(scope="module")
+def full_stack():
+    """Load stack from test data."""
+    return ds.get_needle_data()
+
+
 class TestAlignFunctions:
     """Test alignment functions."""
 
-    def test_apply_shifts_bad_shape(self):
-        stack = ds.get_needle_data()
-        stack = stack.inav[0:5]
+    def test_apply_shifts_bad_shape(self, short_stack):
         shifts = np.zeros(10)
         with pytest.raises(
             ValueError,
             match=r"Number of shifts \(\d+\) is not consistent with number of images "
             r"in the stack \(\d+\)",
         ):
-            etspy.align.apply_shifts(stack, shifts)
+            etspy.align.apply_shifts(short_stack, shifts)
 
-    def test_pad_line(self):
+    def test_pad_line(self, short_stack):
+        claligner = etspy.align.CommonLineAligner(short_stack)
         line = np.zeros(100)
         padding = 200
-        padded = etspy.align.pad_line(line, padding)
+        padded = claligner._pad_line(line, padding)
         assert padded.shape[0] == padding
 
-    def test_pad_line_uneven_line(self):
+    def test_pad_line_uneven_line(self, short_stack):
+        claligner = etspy.align.CommonLineAligner(short_stack)
         line = np.zeros(101)
         padding = 200
-        padded = etspy.align.pad_line(line, padding)
+        padded = claligner._pad_line(line, padding)
         assert padded.shape[0] == padding
 
-    def test_pad_line_uneven_padded(self):
+    def test_pad_line_uneven_padded(self, short_stack):
+        claligner = etspy.align.CommonLineAligner(short_stack)
         line = np.zeros(100)
         padding = 201
-        padded = etspy.align.pad_line(line, padding)
+        padded = claligner._pad_line(line, padding)
         assert padded.shape[0] == padding
 
-    def test_pad_line_uneven_both(self):
+    def test_pad_line_uneven_both(self, short_stack):
+        claligner = etspy.align.CommonLineAligner(short_stack)
         line = np.zeros(101)
         padding = 201
-        padded = etspy.align.pad_line(line, padding)
+        padded = claligner._pad_line(line, padding)
         assert padded.shape[0] == padding
 
-    def test_calc_shifts_cl_no_index(self):
-        stack = ds.get_needle_data()
-        shifts = etspy.align.calc_shifts_cl(stack, None, 0.05, 8)
+    def test_calc_shifts_cl_no_index(self, full_stack):
+        claligner = etspy.align.CommonLineAligner(full_stack)
+        shifts = claligner._calc_shifts_cl(None, 0.05, 8)
         assert isinstance(shifts, np.ndarray)
         assert shifts.shape == (77,)
 
-    def test_calc_shifts_com_with_xrange(self):
-        stack = ds.get_needle_data()
-        shifts_def = etspy.align.calculate_shifts_conservation_of_mass(stack)
-        shifts = etspy.align.calculate_shifts_conservation_of_mass(
-            stack,
-            xrange=(31, 225),
-        )
+    def test_calc_shifts_com_with_xrange(self, full_stack):
+        comaligner = etspy.align.CoMAligner(full_stack)
+        shifts_def = comaligner.calculate_shifts()
+        comaligner = etspy.align.CoMAligner(full_stack, xrange=(31, 225))
+        shifts = comaligner.calculate_shifts()
         assert isinstance(shifts, np.ndarray)
         assert np.all(shifts == shifts_def)
 
-    def test_calc_shifts_stackreg_no_start(self):
-        stack = ds.get_needle_data()
-        shifts = etspy.align.calculate_shifts_stackreg(
-            stack,
+    def test_calc_shifts_stackreg_no_start(self, full_stack):
+        sraligner = etspy.align.StackRegAligner(
+            full_stack,
             start=None,
             show_progressbar=False,
         )
+        shifts = sraligner.calculate_shifts()
         assert isinstance(shifts, np.ndarray)
         assert shifts.shape == (77, 2)
 
-    def test_tilt_com_no_slices_yes_nslices_30_perc(self, caplog):
-        stack = ds.get_needle_data()
-        etspy.align.tilt_com(stack, slices=None, nslices=100)
+    def test_tilt_com_no_slices_yes_nslices_30_perc(self, full_stack, caplog):
+        etspy.align.tilt_com(full_stack, slices=None, nslices=100)
         assert (
             "nslices is greater than 30% of number of x pixels. "
             "Using 76 slices instead."
         ) in caplog.text
 
-    def test_tilt_com_no_slices_yes_nslices_too_big(self):
-        stack = ds.get_needle_data()
+    def test_tilt_com_no_slices_yes_nslices_too_big(self, full_stack):
         with pytest.raises(
             ValueError,
             match=r"nslices is greater than the X-dimension of the data\.",
         ):
-            etspy.align.tilt_com(stack, slices=None, nslices=300)
+            etspy.align.tilt_com(full_stack, slices=None, nslices=300)
 
-    def test_tilt_com_nx_threshold_error(self):
-        stack = ds.get_needle_data()
-        stack = stack.isig[:2, :]
+    def test_tilt_com_nx_threshold_error(self, full_stack):
+        stack = full_stack.isig[:2, :]
         with pytest.raises(
             ValueError,
             match=(
@@ -117,23 +126,22 @@ class TestAlignFunctions:
         ):
             etspy.align.tilt_com(stack)
 
-    def test_calc_shifts_com_cl_res_error(self):
-        stack = ds.get_needle_data()
+    def test_calc_shifts_com_cl_res_error(self, full_stack):
+        claligner = etspy.align.CommonLineAligner(
+            full_stack,
+            com_ref_index=30,
+            cl_resolution=0.9,
+        )
         with pytest.raises(ValueError, match=(r"Resolution should be less than 0.5")):
-            etspy.align.calc_shifts_com_cl(
-                stack,
-                com_ref_index=30,
-                cl_resolution=0.9,
-            )
+            claligner.calculate_shifts()
 
 
 @pytest.mark.skipif(not cupy_in_test_env, reason="cupy not available")
 class TestCUDAAlignFunctions:
     """Test alignment functions using CUDA functionality."""
 
-    def test_stack_reg_pc_cuda(self):
-        stack = ds.get_needle_data()
-        stack.stack_register("PC", cuda=True)
+    def test_stack_reg_pc_cuda(self, short_stack):
+        short_stack.stack_register("PC", cuda=True)
 
     def test_no_cupy(self):
         assert etspy.align.has_cupy
@@ -147,52 +155,22 @@ class TestCUDAAlignFunctions:
 class TestAlignStackRegister:
     """Test alignment using stack reg."""
 
-    def test_register_pc(self):
-        stack = ds.get_needle_data()
-        reg = stack.inav[0:20].stack_register("PC")
+    @pytest.mark.parametrize(
+        "method",
+        [
+            "PC",
+            "StackReg",
+            "COM",
+            "COM-CL",
+        ],
+    )
+    def test_register_methods(self, short_stack, method):
+        reg = short_stack.inav[0:20].stack_register(method)
         assert isinstance(reg, etspy.TomoStack)
-        assert (
-            reg.axes_manager.signal_shape == stack.inav[0:20].axes_manager.signal_shape
-        )
+        assert reg.axes_manager.signal_shape == short_stack.axes_manager.signal_shape
         assert (
             reg.axes_manager.navigation_shape
-            == stack.inav[0:20].axes_manager.navigation_shape
-        )
-
-    def test_register_com(self):
-        stack = ds.get_needle_data()
-        reg = stack.inav[0:20].stack_register("COM")
-        assert isinstance(reg, etspy.TomoStack)
-        assert (
-            reg.axes_manager.signal_shape == stack.inav[0:20].axes_manager.signal_shape
-        )
-        assert (
-            reg.axes_manager.navigation_shape
-            == stack.inav[0:20].axes_manager.navigation_shape
-        )
-
-    def test_register_stackreg(self):
-        stack = ds.get_needle_data()
-        reg = stack.inav[0:20].stack_register("StackReg")
-        assert isinstance(reg, etspy.TomoStack)
-        assert (
-            reg.axes_manager.signal_shape == stack.inav[0:20].axes_manager.signal_shape
-        )
-        assert (
-            reg.axes_manager.navigation_shape
-            == stack.inav[0:20].axes_manager.navigation_shape
-        )
-
-    def test_register_com_cl(self):
-        stack = ds.get_needle_data()
-        reg = stack.inav[0:20].stack_register("COM-CL")
-        assert isinstance(reg, etspy.TomoStack)
-        assert (
-            reg.axes_manager.signal_shape == stack.inav[0:20].axes_manager.signal_shape
-        )
-        assert (
-            reg.axes_manager.navigation_shape
-            == stack.inav[0:20].axes_manager.navigation_shape
+            == short_stack.axes_manager.navigation_shape
         )
 
     def test_register_unknown_method(self):
@@ -206,22 +184,6 @@ class TestAlignStackRegister:
             ),
         ):
             stack.inav[0:20].stack_register(bad_method)  # type: ignore
-
-    def test_align_stack_bad_method(self):
-        """
-        Test invalid method in align_stack directly.
-
-        Since we can't through TomoStack.stack_register.
-        """
-        stack = ds.get_needle_data()
-        bad_method = "WRONG"
-        with pytest.raises(ValueError, match=f"Invalid alignment method {bad_method}"):
-            etspy.align.align_stack(
-                stack,
-                method=bad_method,  # pyright: ignore[reportArgumentType]
-                start=None,
-                show_progressbar=False,
-            )
 
 
 class TestTiltAlign:
